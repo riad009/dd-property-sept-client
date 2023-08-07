dba2e1a1952806419ca188478623df8cc794743e
faysalewucse <faysal.ewucse@gmail.com>
2023-08-07 21:49:33 +0600

    splitting issue solved

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -43,7 +43,7 @@ const Dashboard = () => {
   return (
     <div className="dark:bg-slate-900 min-h-[90vh] p-5 md:p-10 bg-dark2/5">
       <h1 className="font-bold text-3xl">
-        Howdy, {currentUser.name.split(" ")[0]}
+        Howdy, {currentUser?.name?.split(" ")[0]}
       </h1>
       <p className="text-xs">We are glad to see you again</p>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 my-10">
