1da384edbbdd4191ef743a7222237ce67a6f9a6d
kmdshojib <kmd.shojib@gmail.com>
2024-06-15 16:18:23 +0600

    properties has been successfully updated

--- src/pages/dashboard/UpdateProperty.jsx
+++ src/pages/dashboard/UpdateProperty.jsx
@@ -1,117 +1,175 @@
 import React, { useEffect, useState } from "react";
-
 import DashboardHeader from "./DashboardHeader";
-
 import axios from "axios";
-import { Form, Input, Button, Row, Col, Select } from "antd";
+import { Form, Input, Button, Row, Col, Select, Upload } from "antd";
+import { PlusOutlined } from '@ant-design/icons';
 import { useParams } from "react-router-dom";
-import FormInput from "../../components/forms/FormInput";
+import Loader from "../../components/Loader";
+
+const { Option } = Select;
 
 const UpdateProperty = () => {
   const [form] = Form.useForm();
-  // get data from server 
-  const [propertyData, setPropertyData] = useState();
-  const [isLoading, setIsloading] = useState(true);
+  const [propertyData, setPropertyData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isModified, setIsModified] = useState(false);
   const { id } = useParams();
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get(`/property/${id}`)
-        setPropertyData(response?.data);
-        setIsloading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUserData();
-  }, [])
-  const handleUpdateButton = async (e) => {
+  const fetchPropertyData = async () => {
+    setIsLoading(true);
     try {
-      const response = await axios.put(`/update/property/${id}`);
+      const response = await axios.get(`/property/${id}`);
+      setPropertyData(response.data);
+      setIsLoading(false);
     } catch (error) {
-      if (error.response) {
-        console.error("Error Status:", error.response.status);
-        console.error("Error Data:", error.response.data);
-      } else if (error.request) {
-        console.error("No response received:", error.request);
-      } else {
-        console.error("Error message:", error.message);
-      }
+      console.error("Error fetching property data:", error);
+      setIsLoading(false);
     }
   };
-  // destructring property valuses 
+
+  useEffect(() => {
+    fetchPropertyData();
+  }, [id]);
+
+  useEffect(() => {
+    form.setFieldsValue(propertyData);
+    setIsModified(false);
+  }, [propertyData, form]);
+
+  const handleUpdateButton = async () => {
+    setIsLoading(true);
+    const values = form.getFieldsValue();
+    const formData = new FormData();
+
+    for (const key in values) {
+      if (values.hasOwnProperty(key)) {
+        formData.append(key, values[key]);
+      }
+    }
+
+    // Append cover photo file if uploaded
+    if (values.coverPhoto?.fileList?.length) {
+      formData.append('coverImage', values.coverPhoto.fileList[0].originFileObj);
+    }
+
+    // Append other photos files if uploaded
+    if (values.otherPhotos?.fileList?.length) {
+      values.otherPhotos.fileList.forEach(file => {
+        formData.append('imageUrls', file.originFileObj);
+      });
+    }
+
+    try {
+      const res = await axios.put(`/update/property/${id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if (res.status === 201) {
+        await fetchPropertyData();
+        alert(res.data.message);
+      }
+    } catch (error) {
+      console.error("Error updating property:", error.response || error.message);
+      setIsLoading(false);
+    }
+  };
+
+  const handleValuesChange = () => {
+    setIsModified(true);
+  };
+
   if (isLoading) {
-    return <div>Loading</div>
+    return <Loader />;
   }
-  const { propertyName, province, city, location, listingType, price, bedrooms, bathrooms, floorSize, headline, 
-    referenceNote, descriptionEnglish, size, propertyType,rentDuration } = propertyData
-  // update
+
+  const {
+    propertyName, province, city, location, listingType, price, bedrooms,
+    bathrooms, floorSize, headline, video, coverImage = [], imageUrls = [], referenceNote,
+    descriptionEnglish, size, propertyType, rentDuration, contactName, contactEmail,
+    contactNumber, contactAddress, priceType
+  } = propertyData;
+
+  const cover = coverImage.map((url, index) => ({
+    uid: `${-index}`,
+    name: `cover_photo${index}.png`,
+    status: 'done',
+    url,
+  }));
+
+  const images = imageUrls.map((url, index) => ({
+    uid: `${-index}`,
+    name: `other_photo${index}.png`,
+    status: 'done',
+    url,
+  }));
+
   return (
     <Form
       form={form}
       layout="vertical"
-      // initialValues={{
-      //   layout: formLayout,
-      // }}
-
+      initialValues={{
+        propertyName, province, city, location, price, bedrooms, bathrooms,
+        floorSize, headline, video, referenceNote, descriptionEnglish, size,
+        contactName, contactEmail, contactNumber, contactAddress, rentDuration, priceType
+      }}
+      onValuesChange={handleValuesChange}
       className="lg:p-10 p-5 bg-dark2/10"
     >
-      <DashboardHeader
-        title={"Update Property"}
-        description={"We are glad to see you again!"}
-      />
-      {/* Location part */}
-      <div className="mt-5">
-        <div className="bg-white p-10 rounded-lg">
-          <h1 className="mb-5 font-semibold text-2xl">Location</h1>
+      <DashboardHeader title="Update Property" description="We are glad to see you again!" />
 
-          <div className="flex flex-col">
-            <div className="flex flex-col md:flex-row gap-4">
-              <Form.Item
-                label="Name of the property"
-                name="propertyName"
-                className="w-full"
-                rules={[{ required: true }]}
-              >
-                <Input size="large" defaultValue={propertyName} />
-              </Form.Item>
-              <Form.Item label="Province" name="province" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" defaultValue={province} />
-              </Form.Item>
-            </div>
-            <div className="flex flex-col md:flex-row gap-4">
-              <Form.Item label="City" name="city" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" defaultValue={city} />
-              </Form.Item>
-              <Form.Item label="Location" name="location" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" defaultValue={location} />
-              </Form.Item>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Create Listing: Details */}
+      {/* Location Section */}
       <div className="mt-5">
         <div className="bg-white p-10 rounded-lg">
-          <h1 className="mb-5 font-semibold text-2xl">Details</h1>
+          <h1 className="mb-5 font-semibold text-2xl">Update Location</h1>
           <Row gutter={16}>
             <Col xs={24} sm={12}>
-              <Form.Item label="Price" name="price" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" defaultValue={price} />
+              <Form.Item label="Name of the property" name="propertyName" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Province" name="province" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col xs={24} sm={12}>
+              <Form.Item label="City" name="city" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Location" name="location" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+      </div>
+
+      {/* Details Section */}
+      <div className="mt-5">
+        <div className="bg-white p-10 rounded-lg">
+          <h1 className="mb-5 font-semibold text-2xl">Update Details</h1>
+          <Row gutter={16}>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Price" name="price" rules={[{ required: true }]}>
+                <Input size="large" />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12}>
               {listingType === "forSale" || propertyType === "Land" ? (
-                <Form.Item label="TBH" name="TBH" className="w-full" rules={[{ required: true }]}>
-                  <Input size="large" disabled defaultValue="TBH" />
+                <Form.Item label={priceType} name="priceType" rules={[{ required: true }]}>
+                  <Input size="large" disabled />
                 </Form.Item>
               ) : (
-                <Form.Item label="Type" name="type" className="w-full" rules={[{ required: true }]}>
-                  <Select size="large" placeholder="Select Type" defaultValue={rentDuration}>
-                    <Option value="daily">Daily</Option>
-                    <Option value="monthly">Monthly</Option>
-                    <Option value="yearly">Yearly</Option>
+                <Form.Item label="Type" name="rentDuration" rules={[{ required: true }]}>
+                  <Select size="large" placeholder="Select Type">
+                    <Option value="Daily">Daily</Option>
+                    <Option value="Monthly">Monthly</Option>
+                    <Option value="Yearly">Yearly</Option>
                   </Select>
                 </Form.Item>
               )}
@@ -119,8 +177,8 @@ const UpdateProperty = () => {
           </Row>
           <Row gutter={16}>
             <Col xs={24} sm={12}>
-              <Form.Item label="Bedrooms" name="bedrooms" className="w-full" rules={[{ required: true }]}>
-                <Select defaultValue={bedrooms} size="large" placeholder="Select Bedrooms">
+              <Form.Item label="Bedrooms" name="bedrooms" rules={[{ required: true }]}>
+                <Select size="large" placeholder="Select Bedrooms">
                   <Option value="1">1</Option>
                   <Option value="2">2</Option>
                   <Option value="3">3</Option>
@@ -128,8 +186,8 @@ const UpdateProperty = () => {
               </Form.Item>
             </Col>
             <Col xs={24} sm={12}>
-              <Form.Item label="Bathrooms" name="bathrooms" className="w-full" rules={[{ required: true }]}>
-                <Select defaultValue={bathrooms} size="large" placeholder="Select Bathrooms">
+              <Form.Item label="Bathrooms" name="bathrooms" rules={[{ required: true }]}>
+                <Select size="large" placeholder="Select Bathrooms">
                   <Option value="1">1</Option>
                   <Option value="2">2</Option>
                   <Option value="3">3</Option>
@@ -139,47 +197,133 @@ const UpdateProperty = () => {
           </Row>
           <Row gutter={16}>
             <Col xs={24} sm={12}>
-              <Form.Item label="Size m²" name="size" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" placeholder="Size" defaultValue={size} />
+              <Form.Item label="Size m²" name="size" rules={[{ required: true }]}>
+                <Input size="large" />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12}>
-              <Form.Item label="Floor Size" name="floorSize" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" placeholder="Floor Size" defaultValue={floorSize} />
+              <Form.Item label="Floor Size" name="floorSize" rules={[{ required: true }]}>
+                <Input size="large" />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={16}>
             <Col xs={24} sm={12}>
-              <Form.Item label="Reference Note" name="referenceNote" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" placeholder="Reference Note" defaultValue={referenceNote} />
+              <Form.Item label="Reference Note" name="referenceNote" rules={[{ required: true }]}>
+                <Input size="large" />
               </Form.Item>
             </Col>
             <Col xs={24} sm={12}>
-              <Form.Item label="Headline" name="headline" className="w-full" rules={[{ required: true }]}>
-                <Input size="large" placeholder="Headline" defaultValue={headline} />
+              <Form.Item label="Headline" name="headline" rules={[{ required: true }]}>
+                <Input size="large" />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={16}>
             <Col xs={24}>
-              <Form.Item label="Description" name="description" className="w-full" rules={[{ required: true }]}>
-                <Input.TextArea size="large" placeholder="Description" defaultValue={descriptionEnglish} rows={4} />
+              <Form.Item label="Description" name="descriptionEnglish" rules={[{ required: true }]}>
+                <Input.TextArea size="large" rows={4} />
               </Form.Item>
             </Col>
           </Row>
-
-
-          
         </div>
       </div>
+
+      {/* Media Section */}
+      <div className="mt-5">
+        <div className="bg-white p-10 rounded-lg">
+          <h1 className="mb-5 font-semibold text-2xl">Update Listing: Media</h1>
+          <Form.Item
+            name="coverPhoto"
+            label="Add Cover Photo"
+          >
+            <Upload
+              listType="picture-card"
+              maxCount={1}
+              beforeUpload={() => false}
+              defaultFileList={cover}
+            >
+              <div>
+                <PlusOutlined />
+                <div style={{ marginTop: 8 }}>Upload Cover Image</div>
+              </div>
+            </Upload>
+          </Form.Item>
+
+          <Form.Item
+            name="otherPhotos"
+            label="Add Photos"
+          >
+            <Upload
+              listType="picture-card"
+              multiple
+              maxCount={10}
+              beforeUpload={() => false}
+              defaultFileList={images}
+            >
+              <div>
+                <PlusOutlined />
+                <div style={{ marginTop: 8 }}>Upload Image</div>
+              </div>
+            </Upload>
+          </Form.Item>
+
+          <Form.Item
+            name="video"
+            label="Add Video"
+            rules={[
+              {
+                type: 'url',
+                message: 'Please enter a valid URL',
+              },
+            ]}
+          >
+            <Input placeholder="Provide a video link from YouTube" />
+          </Form.Item>
+        </div>
+      </div>
+
+      {/* Contact Information Section */}
+      <div className="mt-5">
+        <div className="bg-white p-10 rounded-lg">
+          <h1 className="mb-5 font-semibold text-2xl">Update Your Contact Information</h1>
+          <Row gutter={16}>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Your name" name="contactName" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Your Email" name="contactEmail" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Your number" name="contactNumber" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item label="Your address" name="contactAddress" rules={[{ required: true }]}>
+                <Input size="large" />
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+      </div>
+
       <div className="flex justify-end">
-        <button
-          className="bg-green-400 text-black  py-2 px-4 rounded-lg mt-4 w-full"
+        <Button
+          type="primary"
+          size="large"
           onClick={handleUpdateButton}
+          disabled={!isModified}
+          className="bg-green-400 text-black py-2 px-4 rounded-lg mt-4 w-full"
         >
           Update
-        </button>
+        </Button>
       </div>
     </Form>
   );
